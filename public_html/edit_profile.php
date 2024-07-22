db306e1d157da5910a7c1b19af0f972afbbe6c61
Robert <gregory2@uwindsor.ca>
2024-07-21 21:55:06 -0400

    Update .gitignore

--- public_html/edit_profile.php
+++ public_html/edit_profile.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'includes/db_config.php'; // Adjust the path if necessary
+include 'includes/db_config.php';
 
 // Check if the user is logged in
 if (!isset($_SESSION['username'])) {
